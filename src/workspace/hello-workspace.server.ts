65511d4400b5c59c8315c9ffe3f5287ededb66db
Kinrany <kinrany@yandex.com>
2019-02-02 23:26:28 +0300

    Add a script that makes everyone say hello

--- src/workspace/hello-workspace.server.ts
+++ src/workspace/hello-workspace.server.ts
@@ -1,1 +1,11 @@
-print('Workspace!');
+import { Chat, Players } from 'rbx-services';
+
+Players.PlayerAdded.Connect(player => {
+  while (true) {
+    wait(5);
+
+    if (player.Character) {
+      Chat.Chat(player.Character, `Hello world! I'm ${player.Name}!`);
+    }
+  }
+});
